37a7106837cfadca1e8b247ec62804dacce77592
Javs69 <oswamtz222@gmail.com>
2026-03-17 22:23:07 -0600

    Cambios no guardados

--- db.php
+++ db.php
@@ -1,13 +1,12 @@
 <?php
-$host = "dpg-d4jk3hngi27c739p6p40-a.oregon-postgres.render.com";
+$host = "localhost";
 $port = "5432";
-$dbname = "utzmweb";
-$user = "utzmweb_user";
-$pass = "Pyxpes0xRMHfiHjjxQbCXqosJP3s7Vn2";
-$sslmode = "require"; // Render requiere TLSs
+$dbname = "UTZMWeb";
+$user = "postgres";
+$pass = "200612";
 
 try {
-  $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;sslmode=$sslmode", $user, $pass);
+  $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
   echo json_encode(["error" => "Error de conexion: " . $e->getMessage()]);
