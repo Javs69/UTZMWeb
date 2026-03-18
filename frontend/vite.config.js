d0a5cafcfefd2760aba4e3bc4efa51d90dbe2ef9
Javs69 <oswamtz222@gmail.com>
2026-03-17 23:22:08 -0600

    Limpieza de archivos obsoletos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/backend': {
-        target: 'http://localhost:8080',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true,
       },
       '/public': {
-        target: 'http://localhost:8080',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true,
       },
     },
